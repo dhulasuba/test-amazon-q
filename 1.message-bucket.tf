2720c90f59c2d58db67b94bd9c98140822650de7
dhulasuba <79701674+dhulasuba@users.noreply.github.com>
2025-07-02 21:29:26 +0530

    Update 1.message-bucket.tf

--- 1.message-bucket.tf
+++ 1.message-bucket.tf
@@ -1,8 +1,4 @@
-#random_string with prefix "name" to generate a unique bucket name
-#need bucket prefix to avoid conflicts
-resource "random_pet" "name" {
-  prefix = "messages-bucket-"
-}
+
 resource "aws_s3_bucket" "messages" {
   bucket = "messages-bucket-${random_pet.name.id}"
   force_destroy = true
@@ -11,4 +7,9 @@ resource "aws_s3_bucket" "messages" {
     Name        = "Messages Bucket"
     Environment = "Production"
   }
+}
+#random_string with prefix "name" to generate a unique bucket name
+#need bucket prefix to avoid conflicts
+resource "random_pet" "name" {
+  prefix = "messages-bucket-"
 }
